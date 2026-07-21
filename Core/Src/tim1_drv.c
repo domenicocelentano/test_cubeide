73638c8fa66377e31680ccc515c72700590715bd
Domenico Celentano <domenicoc@cosmed.it>
2026-07-21 14:32:21 +0200

    implementato IC su TIM1 CH1 PE9

--- Core/Src/tim1_drv.c
+++ Core/Src/tim1_drv.c
@@ -18,11 +18,10 @@ typedef struct _tag_turb
 
 #define LEN 2000
 
-TURB turb[LEN];
-uint16_t pw = 0;
+static volatile TURB turb[LEN];
+static volatile uint16_t pw = 0;
 
-volatile uint16_t capture_old = 0;
-volatile uint16_t period_ticks = 0;
+static volatile uint16_t capture_old = 0;
 
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
 {
@@ -33,13 +32,8 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
 		turb[pw].tick = capture;
 		turb[pw].period = capture - capture_old;
 
-		if ( pw++ >= LEN )
+		if ( ++pw >= LEN )
 			pw = 0;
-
-		float periodo_us = period_ticks * (1e6f / 280000000.0f);
-		float freq = 280000000.0f / period_ticks;
-
-//		uint32_t frequency = HAL_RCC_GetPCLK1Freq() / (period_ticks);
 
 		capture_old = capture;
 	}
